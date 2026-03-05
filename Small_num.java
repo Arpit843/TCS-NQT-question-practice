9e69782f635649e1db29923db5317df9fc648c4a
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-05 18:42:18 +0530

    Add method to find smallest element in array

--- Small_num.java
+++ Small_num.java
@@ -1,1 +1,14 @@
+class Solutions{
+  public static int Array( int[] arr){
+    Arrays.sort(arr);
+    return arr[0];
+  }
+}
 
+public class main{
+  public static void main(String [] args){
+    int[] arr1={5,6,2,1,3,0};
+    System.out.print("smallest element is ",Solutions.Array(arr1));
+}
+
+}
