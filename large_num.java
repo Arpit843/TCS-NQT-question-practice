3a03db72302dbfdad7262a49bea8cf45dbb08f7b
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-06 17:44:30 +0530

    Add method to find largest number in array

--- large_num.java
+++ large_num.java
@@ -1,1 +1,16 @@
 
+import java.util.Arrays;
+
+class Solutions{
+    public static int sortarr(int []arr){
+        Arrays.sort(arr);
+        return arr[arr.length -1];
+    }
+}
+public static void main (String []args){
+    int [] arr1 ={5,6,2,1,3,0};
+    int [] arr2 ={5,6,2,8,9};
+
+    System.out.println("largest num is "+ Solutions.sortarr(arr1));
+    System.out.println("largest num is "+ Solutions.sortarr(arr2));
+}
