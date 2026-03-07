d8b85500ee3d4d5d825adff8d91e2c6e9852bc16
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-07 22:39:12 +0530

    reverseArray final implementation
    Fix reverseArray method to correctly reverse the array.

--- reverse_arr.java
+++ reverse_arr.java
@@ -1,7 +1,13 @@
-
 import java.util.*;
 
 public class reverseArray(int [] arr){
+  int i =arr[0];
+  int j = arr.length
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = arr[temp];
+  i++;
+  j--;
   
 }
 
@@ -9,7 +15,13 @@ public static void main(String []args){
   int [] ar1= {5,6,7,1,3,4,8};
 
   for (int i=0; i <ar1.length; i++){
-    System.out.print()
+    System.out.print(ar1[i]);
   }
   reverseArray(ar1);
+
+  System.out.println();
+  
+  for (int i=0; i <ar1.length; i++){
+    System.out.print(ar1[i]);
+  }
 }
