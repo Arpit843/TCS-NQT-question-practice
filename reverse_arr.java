4554caf55dc520578cc06a4fe8773de873a264b2
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-07 22:34:50 +0530

    Add reverseArray method and main function
    Implement reverseArray method and main function structure.

--- reverse_arr.java
+++ reverse_arr.java
@@ -1,1 +1,15 @@
 
+import java.util.*;
+
+public class reverseArray(int [] arr){
+  
+}
+
+public static void main(String []args){
+  int [] ar1= {5,6,7,1,3,4,8};
+
+  for (int i=0; i <ar1.length; i++){
+    System.out.print()
+  }
+  reverseArray(ar1);
+}
