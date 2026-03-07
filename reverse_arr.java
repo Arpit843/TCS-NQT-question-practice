c3d0ca95edc56ef3c960c2fd3e637c18436d1f1d
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-07 22:53:04 +0530

    Fix reverse array implementation and structure

--- reverse_arr.java
+++ reverse_arr.java
@@ -1,27 +1,32 @@
 import java.util.*;
-
-public class reverseArray(int [] arr){
-  int i =arr[0];
-  int j = arr.length
-  int temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = arr[temp];
-  i++;
-  j--;
-  
+class Solution{
+    public void reverseArray(int [] arr){
+        int i =0;
+        int j = arr.length-1;
+        while(i< j){
+            
+            int temp = arr[i];
+              arr[i] = arr[j];
+              arr[j] = temp;
+              i++;
+              j--;
+}}
 }
-
-public static void main(String []args){
-  int [] ar1= {5,6,7,1,3,4,8};
-
-  for (int i=0; i <ar1.length; i++){
-    System.out.print(ar1[i]);
-  }
-  reverseArray(ar1);
-
-  System.out.println();
-  
-  for (int i=0; i <ar1.length; i++){
-    System.out.print(ar1[i]);
-  }
+public class Main{
+    
+    public static void main(String []args){
+      int [] ar1= {5,6,7,1,3,4,8};
+      Solution obj = new Solution();
+    
+      for (int i=0; i <ar1.length; i++){
+        System.out.print(ar1[i]);
+      }
+      obj.reverseArray(ar1);
+    
+      System.out.println();
+      
+      for (int i=0; i <ar1.length; i++){
+        System.out.print(ar1[i]);
+      }
+    }
 }
