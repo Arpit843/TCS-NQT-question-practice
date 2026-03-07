559a4d2b965789d74c85eafa0600cbec255f39e1
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-07 22:14:27 +0530

    find second smallest or second largest number in the arrar
    Implement initial structure for finding second largest and smallest numbers in an array.

---
+++ second_L_orsmall.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+public class Second_L_orsmall {
+    //for an array of 2 elements
+    if (n==0 || n==1){
+        System.out.println();
+
+        
+    }
+}
+public static void main (String [] args){
+    int [] arr1={4,5,2,3,1,7};
+    n= arr.length;
+    System.out.println("second largest and second smallest number is"+ Second_L_orsmall.sortarr(arr1));
+
+}
