170a83b5ada90fe2c89dafedb1e57d1a07b38dbf
Arpit Shakkerwal <112974894+Arpit843@users.noreply.github.com>
2026-03-10 19:00:31 +0530

    Add prime number check logic to primecheck.java

--- primecheck.java
+++ primecheck.java
@@ -8,3 +8,20 @@ public class PrimeCheck {
 
         boolean isPrime = true;
 
+        if (n <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime)
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
